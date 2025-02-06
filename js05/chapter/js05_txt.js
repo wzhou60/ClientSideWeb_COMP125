9fafc32a83f0f77dfa864ba27ce6cfb1959ad30f
dillcheese <dillcheese1@gmail.com>
2025-02-06 12:38:03 -0500

    wk5 class 2

--- js05/chapter/js05_txt.js
+++ js05/chapter/js05_txt.js
@@ -10,7 +10,110 @@
       Filename: js05.js
 */
 
-window.addEventListener("load", setupGallery);
+window.addEventListener("load", creatLightbox);
+
+function creatLightbox() {
+  //Lightbox Container
+  let lightBox = document.getElementById("lightbox");
+  //Parts of the lightbox
+  let lbTitle = document.createElement("h1");
+  let lbCounter = document.createElement("div");
+  let lbPrev = document.createElement("div");
+  let lbNext = document.createElement("div");
+  let lbPlay = document.createElement("div");
+  let lbImages = document.createElement("div");
+
+  //let lbImages = document.createElement("div");
+  //Design the lightbox title
+  lightBox.appendChild(lbTitle);
+  lbTitle.id = "lbTitle";
+  lbTitle.textContent = lightboxTitle;
+
+  //Design the lightbox slide counter
+  lightBox.appendChild(lbCounter);
+  lbCounter.id = "lbCounter";
+  let currentImg = 1;
+  lbCounter.textContent = currentImg + " / " + imgCount;
+
+  //Design the lightbox previous slide button
+  lightBox.appendChild(lbPrev);
+  lbPrev.id = "lbPrev";
+  lbPrev.innerHTML = "&#9664";
+  lbPrev.onclick = showPrev;
+
+  //Design the lightbox next slide button
+  lightBox.appendChild(lbNext);
+  lbNext.id = "lbNext";
+  lbNext.innerHTML = "&#9654;";
+  lbNext.onclick = showNext;
+
+  //Design the lightbox Play-Pause button
+  lightBox.appendChild(lbPlay);
+  lbPlay.id = "lbPlay";
+  lbPlay.innerHTML = "&#9199;";
+  let timeID;
+  lbPlay.onclick = function () {
+    if (timeID) {
+      clearInterval(timeID);
+      timeID = null;
+    } else {
+      showNext();
+      timeID = window.setInterval(showNext, 1500);
+    }
+  };
+
+  //Design the lightbox images container
+  lightBox.appendChild(lbImages);
+  lbImages.id = "lbImages";
+  //Add images from the imgFiles array to the container
+  for (let i = 0; i < imgCount; i++) {
+    let image = document.createElement("img");
+    image.src = imgFiles[i];
+    image.alt = imgCaptions[i];
+    image.onclick = createOverlay;
+    lbImages.appendChild(image);
+  }
+
+  function showNext() {
+    lbImages.appendChild(lbImages.firstElementChild);
+    currentImg < imgCount ? currentImg++ : (currentImg = 1);
+    lbCounter.textContent = currentImg + " / " + imgCount;
+  }
+  function showPrev() {
+    lbImages.insertBefore(lbImages.lastElementChild, lbImages.firstElementChild);
+    currentImg > 1 ? currentImg-- : (currentImg = imgCount);
+    lbCounter.textContent = currentImg + " / " + imgCount;
+  }
+
+  function createOverlay() {
+    let overlay = document.createElement("div");
+    overlay.id = "lbOverlay";
+    //Add the figure box to the overlay
+    let figureBox = document.createElement("figure");
+    overlay.appendChild(figureBox);
+
+    //add image to the figure box
+    let overlayImage = this.cloneNode(true);
+    figureBox.appendChild(overlayImage);
+
+    //add caption to image
+    let overlayCaption = document.createElement("figcaption");
+    overlayCaption.textContent = overlayImage.alt;
+    figureBox.appendChild(overlayCaption);
+
+    //add a close button
+    let closeButton = document.createElement("div");
+    closeButton.id = "lbOverlayClose";
+    closeButton.innerHTML = "&times;";
+    closeButton.onclick = function () {
+      document.body.removeChild(overlay);
+    }
+    overlay.appendChild(closeButton);
+
+    document.body.appendChild(overlay);
+  }
+}
+/* window.addEventListener("load", setupGallery);
 
 function setupGallery() {
    let imageCount = imgFiles.length;
@@ -122,4 +225,4 @@ function setupGallery() {
       document.body.appendChild(modalWindow);
    }
    
-}
+} */
